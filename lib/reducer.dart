0311672bd624bab69088ce759a99bb7d4921969a
tabe <tabetabe.unity@gmail.com>
2020-03-14 22:21:56 +0900

    fix root reducer

--- lib/reducer.dart
+++ lib/reducer.dart
@@ -5,15 +5,15 @@ import 'main.dart';
 import 'action.dart';
 
 //Reducer
-AppState reducer(AppState prev, action) {
+Future<AppState> reducer(AppState prev, action) async {
   print(action);
   if (action is AddTaskAction) {
-    return AppState(addTaskReducer(prev, action.newTask));
+    return AppState(await addTaskReducer(prev, action.newTask));
   } else if (action is DeleteTaskAction) {
-    return AppState(deleteTaskReducer(prev, action.deleteIndex));
+    return AppState(await deleteTaskReducer(prev, action.deleteIndex));
   } else if (action is EditTaskAction) {
     return AppState(
-        editTaskReducer(prev, action.editedTask, action.editedTaskIndex));
+        await editTaskReducer(prev, action.editedTask, action.editedTaskIndex));
   }
 }
 
@@ -49,6 +49,13 @@ Future<List<Map<String, dynamic>>> editTaskReducer(
   final String updateSql =
       "UPDATE TodoList SET fuga='$editedTask' WHERE id=$editedTaskId";
   db.rawUpdate(updateSql);
+  final List<Map<String, dynamic>> result =
+      await db.rawQuery("SELECT * FROM TodoList");
+  return result;
+}
+
+Future<List<Map<String, dynamic>>> syncTaskReducer(AppState prev) async {
+  Database db = await DbProvider().db;
   final List<Map<String, dynamic>> result =
       await db.rawQuery("SELECT * FROM TodoList");
   return result;
